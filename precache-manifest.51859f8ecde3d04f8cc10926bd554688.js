51598078896e489a81d543fb31e506990140be09
Max Tsh <tatshahdoost@gmail.com>
2020-04-24 13:50:16 +0430

    Updates

---
+++ precache-manifest.51859f8ecde3d04f8cc10926bd554688.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51668c5a05d73fd93ee9790e827eeeb8",
+    "url": "/CoVID-19-React/index.html"
+  },
+  {
+    "revision": "3c8f5705b72c847305f2",
+    "url": "/CoVID-19-React/static/css/main.3d869874.chunk.css"
+  },
+  {
+    "revision": "aadbdf693e92aefb2fe5",
+    "url": "/CoVID-19-React/static/js/2.5b1614fc.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/CoVID-19-React/static/js/2.5b1614fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8f5705b72c847305f2",
+    "url": "/CoVID-19-React/static/js/main.8a5702f0.chunk.js"
+  },
+  {
+    "revision": "946f3bdb8f3cbb0bb39d",
+    "url": "/CoVID-19-React/static/js/runtime-main.524441e8.js"
+  },
+  {
+    "revision": "4ea58bd79f5f8f8a3e3362d6bb9ef24a",
+    "url": "/CoVID-19-React/static/media/White_Intro.4ea58bd7.png"
+  },
+  {
+    "revision": "88f9663c101763a8a2007624cb2726a3",
+    "url": "/CoVID-19-React/static/media/virus.88f9663c.jpg"
+  }
+]);
